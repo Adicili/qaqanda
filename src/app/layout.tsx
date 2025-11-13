cafa807640c3276765c8ccebfe43e5609ba882c6
Adicili <adierkocevic@gmail.com>
2025-11-13 12:02:00 +0100

    reverted test changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import React from 'react';
-import { useState } from 'react'; // dupli/unused import
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
