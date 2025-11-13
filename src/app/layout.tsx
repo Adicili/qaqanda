a9c318ed19db1cd48e1182e5061d2803f284855c
Adicili <adierkocevic@gmail.com>
2025-11-13 11:52:13 +0100

    test: provoke lint fail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import React from 'react';
+import { useState } from 'react'; // dupli/unused import
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
